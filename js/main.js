d160282a316aa881a2f59a5ae935fb27279c4c9a
amandapadilla <amandapadillasantiago@gmail.com>
2019-07-29 16:37:00 +0200

    create main.js + add randomNumber function

---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+  const myRandomNumber = getRandomNumber(100);
+  
+  console.log(`Mi número aleatorio es ${myRandomNumber}`);
