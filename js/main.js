d7c1287d7d90bd75ab56a1b5746e57e8c5990bd1
amandapadilla <amandapadillasantiago@gmail.com>
2019-07-29 17:27:53 +0200

    add loop, conditional and event in comments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
 let randomNumber = getRandomNumber;
-let inputNumber = document.getElementById("inputNumber");
-const compareInputSubmit = document.querySelector("inputSubmit");
+let inputNumber = document.getElementById("#inputNumber");
+const compareInputSubmit = document.querySelector(".inputSubmit");
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
@@ -11,17 +11,28 @@ const myRandomNumber = getRandomNumber(100);
 
 console.log(`Mi número aleatorio es ${myRandomNumber}`);
 
-if (inputNumber = randomNumber){
-    answerResult.innerHTML="¡ACERTASTE! Has ganado, campeona.";
-} else if (inputNumber < randomNumber) {
-    answerResult.innerHTML="Ooh...demasiado BAJO. Prueba otra vez.";
-} else (inputNumber > randomNumber) {
-    answerResult.innerHTML="Ooh...demasiado ALTO. Prueba otra vez.";
-}
+function compareInputValue {
+    if (inputNumber = randomNumber){
+    return `answerResult.innerHTML="¡ACERTASTE! Has ganado, campeona.";`
+    } else if (inputNumber < randomNumber) {
+    return `answerResult.innerHTML="Ooh...demasiado BAJO. Prueba otra vez."`;
+    } else (inputNumber > randomNumber) {
+    return 
+    `answerResult.innerHTML="Ooh...demasiado ALTO. Prueba otra vez."`;
+    }
+};
 
-//Falta el evento para realizar la comparación. No se ha completado por falta de tiempo.
+//Falta el evento para realizar la comparación. No se ha completado por falta de tiempo. Sería algo así: 
+//const compareInputSubmit = 
+
+//compare.addEventListener('click', function compareInputValue(){
+//   answerResult.innerHTML = ???
+//};
+
+ 
 
 //Falta programar el bucle del contador, por falta de tiempo. Sería algo así: 
+
 // for (let i=1; i<101; i++){
  //   counterNumber.innerHTML = (number + i);
 //}
