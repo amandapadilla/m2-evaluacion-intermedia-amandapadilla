43ad06bab51b68daad4cf90e88eb25e742834f94
amandapadilla <amandapadillasantiago@gmail.com>
2019-08-03 21:53:35 +0200

    finish exercise and fix errors

--- js/main.js
+++ js/main.js
@@ -1,38 +1,32 @@
 "use strict";
-
-let randomNumber = getRandomNumber;
-let inputNumber = document.getElementById("#inputNumber");
-const compareInputSubmit = document.querySelector(".inputSubmit");
-
+//crear una función para generar una número aleatorio
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-const myRandomNumber = getRandomNumber(100);
+let randomNumber = getRandomNumber(100);
 
-//console.log(`Mi número aleatorio es ${myRandomNumber}`);
+//Recoger el número aleatorio y crear una variable para realizar la comparación con el input
+let inputNumber = document.querySelector(".js-inputNumber");
+let displayNumber = document.querySelector(".js-counterNumber");
+let counterNumber = 0;
+let answerResult = document.querySelector(".js-answerResult");
 
-function compareInputValue {
-    if (inputNumber = randomNumber){
-    return `answerResult.innerHTML="¡ACERTASTE! Has ganado, campeona.";`
-    } else if (inputNumber < randomNumber) {
-    return `answerResult.innerHTML="Ooh...demasiado BAJO. Prueba otra vez."`;
-    } else (inputNumber > randomNumber) {
-    return 
-    `answerResult.innerHTML="Ooh...demasiado ALTO. Prueba otra vez."`;
-    }
+//Declarar función con condicional para devolver un mensaje e incrementar el contador cada vez que se introduzca un número y se haga click en submit
+const handleClick = function(ev) {
+  ev.preventDefault();
+  if (inputNumber.value == randomNumber) {
+    answerResult.innerHTML = "¡ACERTASTE! Has ganado, campeona.";
+  } else if (inputNumber.value < randomNumber) {
+    answerResult.innerHTML = "Ooh...demasiado BAJO. Prueba otra vez.";
+  } else if (inputNumber.value > randomNumber) {
+    answerResult.innerHTML = "Ooh...demasiado ALTO. Prueba otra vez.";
+  } else {
+    answerResult.innerHTML = "¡OHHH NOOO! Este número no es válido para jugar.";
+  }
+  counterNumber++;
+  displayNumber.innerHTML = counterNumber;
 };
 
-//Falta el evento para realizar la comparación. No se ha completado por falta de tiempo. Sería algo así: 
-//const compareInputSubmit = 
-
-//compare.addEventListener('click', function compareInputValue(){
-//   answerResult.innerHTML = ???
-//};
-
- 
-
-//Falta programar el bucle del contador, por falta de tiempo. Sería algo así: 
-
-// for (let i=1; i<101; i++){
- //   counterNumber.innerHTML = (number + i);
-//}
+const compareInputSubmit = document.querySelector(".inputSubmit");
+//Llamada al evento para realizar la comparación.
+compareInputSubmit.addEventListener("click", handleClick);
